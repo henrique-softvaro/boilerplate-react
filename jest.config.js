5143124c6cb003f86c6befd5bebf2397ff536b79
Henrique braga leao <henrique@MacBook-Pro.local>
2021-09-29 15:35:48 -0300

    fix: change husky version

--- jest.config.js
+++ jest.config.js
@@ -14,12 +14,10 @@ module.exports = {
     '@/(.*)': '<rootDir>/src/$1',
   },
   coverageThreshold: {
-    global: {
-      branches: 85,
-      functions: 85,
-      lines: 85,
-      statements: 85,
-    },
+    branches: 85,
+    functions: 85,
+    lines: 85,
+    statements: 85,
   },
   setupFilesAfterEnv: ['./config/jest/setupTests.ts'],
 };
