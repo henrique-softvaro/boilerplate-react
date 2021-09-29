50b1f51cda492f5c6b705232e02bb6792bf1d799
Henrique braga leao <henrique@MacBook-Pro.local>
2021-09-29 17:09:08 -0300

    refactor: added config to jest.config

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,13 @@
 module.exports = {
   roots: ['<rootDir>/src'],
+  collectCoverage: true,
   collectCoverageFrom: [
     '<rootDir>/src/**/*.{ts,tsx}',
-    '!**/*.d.ts',
-    '!<rootDir>/src/**/index.ts',
+    '!**/**/*.d.ts',
+    '!<rootDir>/src/index.tsx',
+    '!<rootDir>/src/App.tsx',
   ],
+  testPathIgnorePatterns: ['node_modules', '<rootDir>/src/index.tsx'],
   coverageDirectory: 'coverage',
   testEnvironment: 'jsdom',
   transform: {
@@ -14,10 +17,12 @@ module.exports = {
     '@/(.*)': '<rootDir>/src/$1',
   },
   coverageThreshold: {
-    branches: 85,
-    functions: 85,
-    lines: 85,
-    statements: 85,
+    global: {
+      branches: 85,
+      functions: 85,
+      lines: 85,
+      statements: 85,
+    },
   },
   setupFilesAfterEnv: ['./config/jest/setupTests.ts'],
 };
--- src/components/Button/index.spec.tsx
+++ src/components/Button/index.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { screen, render } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 import { Button } from '.';
 
